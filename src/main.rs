6c79f7d8e6c72bcfd8f762edff03bb1eac4bd22e
hero <staroomoostar@gmail.com>
2024-06-03 22:09:16 +0800

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-fn main() {
-    let (a, b) = (1, 2);
-    let r = add_two_i32(a, b);
-    println!("result is :{}", r);
-}
+fn main() {}
 
 fn add_two_i32(a: i32, b: i32) -> i32 {
     return a + b;

575b9d45b93468d106521127d68f8006b1b55776
hero <staroomoostar@gmail.com>
2024-06-03 22:04:25 +0800

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let (a, b) = (1, 2);
+    let r = add_two_i32(a, b);
+    println!("result is :{}", r);
+}
+
+fn add_two_i32(a: i32, b: i32) -> i32 {
+    return a + b;
 }
 
 #[cfg(test)]
 mod test {
+    use crate::add_two_i32;
+
     #[test]
-    fn test_hello() {
-        println!("success");
+    fn test_add_two_i32() {
+        let (a, b) = (1, 2);
+        let r = add_two_i32(a, b);
+        assert_eq!(r, 3);
     }
 }
